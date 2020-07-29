3e3f2fb24d53477df5ab22d6dab7f4cb0215700c
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-07-29 10:47:07 +0900

    First Commit

---
+++ Pages/TextAnalytics.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KomiyasaWebApp.Pages
+{
+    public class TextAnalyticsModel : PageModel
+    {
+        //Text Analytics で処理をするプロパティを
+        public string inputText { get; set; }
+        public int Answer { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
+
+    
